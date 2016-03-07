41c2fcca81133d5cae18c7c86b9bac32624dc5f0
robertobernabe <Fs9L9iaC>
2016-03-07 15:24:28 +0100

    Added xsd and example xml

--- junit-report-creator/junit_report_creator/junit.py
+++ junit-report-creator/junit_report_creator/junit.py
@@ -1,3 +1,71 @@
+"""
+junit.rnc:
+#----------------------------------------------------------------------------------
+start = testsuite
+
+property = element property {
+   attribute name {text},
+   attribute value {text}
+}
+
+properties = element properties {
+   property*
+}
+
+failure = element failure {
+   attribute message {text},
+   attribute type {text},
+   text
+}
+
+testcase = element testcase {
+   attribute classname {text},
+   attribute name {text},
+   attribute time {text},
+   failure?
+}
+
+testsuite = element testsuite {
+   attribute errors {xsd:integer},
+   attribute failures {xsd:integer},
+   attribute hostname {text},
+   attribute name {text},
+   attribute tests {xsd:integer},
+   attribute time {xsd:double},
+   attribute timestamp {xsd:dateTime},
+   properties,
+   testcase*,
+   element system-out {text},
+   element system-err {text}
+}
+#----------------------------------------------------------------------------------
+
+
+and junitreport.rnc
+#----------------------------------------------------------------------------------
+include "junit.rnc" {
+   start = testsuites
+   testsuite = element testsuite {
+      attribute errors {xsd:integer},
+      attribute failures {xsd:integer},
+      attribute hostname {text},
+      attribute name {text},
+      attribute tests {xsd:integer},
+      attribute time {xsd:double},
+      attribute timestamp {xsd:dateTime},
+      attribute id {text},
+      attribute package {text},
+      properties,
+      testcase*,
+      element system-out {text},
+      element system-err {text}
+   }
+}
+
+testsuites = element testsuites {
+   testsuite*
+}
+"""
 import math
 import unittest
 from datetime import timedelta
@@ -25,124 +93,68 @@ class TestResult(unittest.TestResult):
         return ': '.join(err)
 
 
-class TestCase(unittest.TestCase):
-    TR_CLASS = TestResult
-    stdout = None
-    stderr = None
+class TestCase(object):
+    """
+    testcase = element testcase {
+    attribute classname {text},
+    attribute name {text},
+    attribute time {text},
+    failure?
+    }
 
-    def __init__(self, classname, methodname):
-        super(TestCase, self).__init__()
+    """
+
+    def __init__(self, classname, name):
         self.classname = classname
-        self.methodname = methodname
+        self.name = name
+        self.failure = None
+        self.skipped = None
+        self.error = None
 
     def __str__(self):
-        return "%s (%s)" % (self.methodname, self.classname)
+        return "%s (%s)" % (self.classname, self.name)
 
-    def __repr__(self):
-        return "<%s testMethod=%s>" % \
-               (self.classname, self.methodname)
 
-    def __hash__(self):
-        return hash((type(self), self.classname, self.methodname))
-
-    def id(self):
-        return "%s.%s" % (self.classname, self.methodname)
-
-    def seed(self, result, typename=None, message=None, trace=None):
-        """ Provide the expected result """
-        self.result, self.typename, self.message, self.trace = result, typename, message, trace
-
-    def run(self, tr=None):
-        """ Fake run() that produces the seeded result """
-        tr = tr or self.TR_CLASS()
-
-        tr.startTest(self)
-        if self.result == 'success':
-            tr.addSuccess(self)
-        elif self.result == 'skipped':
-            tr.addSkip(self, '%s: %s' % (self.typename, self._textMessage()))
-        elif self.result == 'error':
-            tr.addError(self, (self.typename, self._textMessage()))
-        elif self.result == 'failure':
-            tr.addFailure(self, (self.typename, self._textMessage()))
-        tr.stopTest(self)
-
-        return tr
-
-    def _textMessage(self):
-        msg = (e for e in (self.message, self.trace) if e)
-        return '\n\n'.join(msg) or None
-
-    @property
-    def alltext(self):
-        err = (e for e in (self.typename, self.message) if e)
-        err = ': '.join(err)
-        txt = (e for e in (err, self.trace) if e)
-        return '\n\n'.join(txt) or None
-
-    def setUp(self):
-        """ Dummy method so __init__ does not fail """
-        pass
-
-    def tearDown(self):
-        """ Dummy method so __init__ does not fail """
-        pass
-
-    def runTest(self):
-        """ Dummy method so __init__ does not fail """
-        self.run()
-
-    @property
-    def basename(self):
-        return self.classname.rpartition('.')[2]
-
-    @property
-    def success(self):
-        return self.result == 'success'
-
-    @property
-    def skipped(self):
-        return self.result == 'skipped'
-
-    @property
-    def failed(self):
-        return self.result == 'failure'
-
-    @property
-    def errored(self):
-        return self.result == 'error'
-
-    @property
-    def good(self):
-        return self.skipped or self.success
-
-    @property
-    def bad(self):
-        return not self.good
-
-    @property
-    def stdall(self):
-        """ All system output """
-        return '\n'.join([out for out in (self.stdout, self.stderr) if out])
 
 
 class TestSuite(object):
+    """
+    testsuite = element testsuite {
+       attribute errors {xsd:integer},
+       attribute failures {xsd:integer},
+       attribute hostname {text},
+       attribute name {text},
+       attribute tests {xsd:integer},
+       attribute time {xsd:double},
+       attribute timestamp {xsd:dateTime},
+       properties,
+       testcase*,
+       element system-out {text},
+       element system-err {text}
+    }
+    """
     def __init__(self, *args, **kwargs):
+        self.errors = 0
+        self.failures = 0
+        self.tests = 0
+        self.time = 0.0
+        self.timestamp = None
+        self.hostname = 'localhost'
+        self.name = None
         self.properties = {}
         self.stdout = None
         self.stderr = None
-        self._tests = []
+        self.testcase = []
 
-    def addTest(self, test):
-        self._tests.append(test)
+    def __str__(self):
+        return "Testsuite name: {0} ".format(self.name)
 
-    def run(self, a):
-        pass
+    def addTestcase(self, testcase):
+        self.testcase.append(testcase)
+
+
 
 class Parser(object):
-    TC_CLASS = TestCase
-    TS_CLASS = TestSuite
-    TR_CLASS = TestResult
 
     def parse(self, source):
         xml = ElementTree.parse(source)
@@ -150,36 +162,46 @@ class Parser(object):
         return self.parse_root(root)
 
     def parse_root(self, root):
-        ts = self.TS_CLASS()
+        testsuites = []
         if root.tag == 'testsuites':
             for subroot in root:
-                self.parse_testsuite(subroot, ts)
+                ts = self.parse_testsuite(subroot)
+                testsuites.append(ts)
         else:
-            self.parse_testsuite(root, ts)
+            ts = self.parse_testsuite(root)
+            testsuites.append(ts)
 
-        tr = ts.run(self.TR_CLASS())
+        # tr = ts.run(self.TR_CLASS())
 
-        tr.time = to_timedelta(root.attrib.get('time'))
+        # tr.time = to_timedelta(root.attrib.get('time'))
 
         # check totals if they are in the root XML element
-        if 'errors' in root.attrib:
-            assert len(tr.errors) == int(root.attrib['errors'])
-        if 'failures' in root.attrib:
-            assert len(tr.failures) == int(root.attrib['failures'])
-        if 'skip' in root.attrib:
-            assert len(tr.skipped) == int(root.attrib['skip'])
+        # if 'errors' in root.attrib:
+        #     assert len(tr.errors) == int(root.attrib['errors'])
+        # if 'failures' in root.attrib:
+        #     assert len(tr.failures) == int(root.attrib['failures'])
+        # if 'skip' in root.attrib:
+        #     assert len(tr.skipped) == int(root.attrib['skip'])
         if 'tests' in root.attrib:
-            testAmount = len(list(ts))
-            testAmountAttrib = int(root.attrib['tests'])
-            log.debug("Attrib tests %s doesnt match amount of testcases %s", testAmountAttrib, testAmount)
+            testsCount = len(list(ts.testcase))
+            testsCountAttrib = int(root.attrib['tests'])
+            log.debug("Attrib tests %s doesnt match count of testcases %s", testsCountAttrib, testsCount)
             # assert len(list(ts)) == int(root.attrib['tests']), "Attrib tests doesnt match amount of testcases"
 
-        return (ts, tr)
+        return testsuites
 
-    def parse_testsuite(self, root, ts):
+    def parse_testsuite(self, root):
         assert root.tag == 'testsuite'
+        ts = TestSuite()
         ts.name = root.attrib.get('name')
         ts.package = root.attrib.get('package')
+        ts.errors = root.attrib.get('errors')
+        ts.skipped = root.attrib.get('skipped')
+        ts.tests = root.attrib.get('tests')
+        ts.failures = root.attrib.get('failures')
+        ts.time = root.attrib.get('time')
+        ts.timestamp = root.attrib.get('timestamp')
+
         for el in root:
             if el.tag == 'testcase':
                 self.parse_testcase(el, ts)
@@ -189,18 +211,18 @@ class Parser(object):
                 ts.stdout = el.text.strip()
             if el.tag == 'system-err' and el.text:
                 ts.stderr = el.text.strip()
+        return ts
 
     def parse_testcase(self, el, ts):
         tc_classname = el.attrib.get('classname') or ts.name
-        tc = self.TC_CLASS(tc_classname, el.attrib['name'])
-        tc.seed('success', trace=el.text or None)
+        tc = TestCase(tc_classname, el.attrib['name'])
         tc.time = to_timedelta(el.attrib.get('time'))
         message = None
         text = None
         for e in el:
             # error takes over failure in JUnit 4
             if e.tag in ('failure', 'error', 'skipped'):
-                tc = self.TC_CLASS(tc_classname, el.attrib['name'])
+                tc = TestCase(tc_classname, el.attrib['name'])
                 result = e.tag
                 typename = e.attrib.get('type')
 
@@ -208,7 +230,11 @@ class Parser(object):
                 message = e.attrib.get('message') or message
                 text = e.text or text
 
-                tc.seed(result, typename, message, text)
+                setattr(tc, result, {'message': message,
+                                     'type': typename,
+                                     'text': text})
+
+                # tc.seed(result, typename, message, text)
                 tc.time = to_timedelta(el.attrib.get('time'))
             if e.tag == 'system-out' and e.text:
                 tc.stdout = e.text.strip()
@@ -216,7 +242,7 @@ class Parser(object):
                 tc.stderr = e.text.strip()
 
         # add either the original "success" tc or a tc created by elements
-        ts.addTest(tc)
+        ts.addTestcase(tc)
 
     def parse_properties(self, el, ts):
         for e in el:
@@ -230,5 +256,7 @@ def parse(source):
 
 
 if __name__ == '__main__':
-    t, tt = parse("../report.xml")
-    print(t, tt)
+    testsuites = parse("../report.xml")
+    for testsuite in testsuites:
+        for tc in testsuite.testcase:
+            print(tc)
